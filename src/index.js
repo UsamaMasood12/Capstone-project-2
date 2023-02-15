430b889eb54f7a85c33331d595ecb3179df1552b
Usama Masood <umasood.bee17seecs@seecs.edu.pk>
2023-02-15 12:43:47 +0500

    addition of like button

--- src/index.js
+++ src/index.js
@@ -1,23 +1,40 @@
-import fetchSeason from './modules/APIfunctions.js';
+import { fetchSeason, fetchLike, postNewLike } from './modules/APIfunctions.js';
 import './style.css';
 
 const seasons = ['130070', '58465', '58464'];
 
 window.onload = () => {
   fetchSeason(seasons[0]);
+  fetchLike();
 };
 
 const seasonbtn1 = document.querySelector('#season-1');
 seasonbtn1.onclick = () => {
   fetchSeason(seasons[0]);
+  fetchLike();
 };
 
 const seasonbtn2 = document.querySelector('#season-2');
 seasonbtn2.onclick = () => {
   fetchSeason(seasons[1]);
+  fetchLike();
 };
 
 const seasonbtn3 = document.querySelector('#season-3');
 seasonbtn3.onclick = () => {
   fetchSeason(seasons[2]);
+  fetchLike();
+};
+
+const episodesContainer = document.querySelector('.home-page');
+episodesContainer.onclick = (event) => {
+  if (event.target.id === 'heart-button') {
+    const button = event.target;
+    button.classList.toggle('heart-animation');
+    const { episodeId } = event.target.dataset;
+    postNewLike(episodeId);
+    setTimeout(() => {
+      button.classList.toggle('heart-animation');
+    }, 2500);
+  }
 };
--- src/modules/homepage.js
+++ src/modules/homepage.js
@@ -13,8 +13,21 @@ export const createSeasonList = (episodeDetails) => {
     <img class="episode-image" src="${episodeDetail.image.medium}" alt="episode image">
     <div class="header-wrapper">
     <h3 class="episode-name">${episodeDetail.name}</h3>
+    <div class="likes-wrapper">
+    <p><i class="fa fa-heart" id="heart-button" data-episode-Id=${episodeDetail.id}></i></p>
+    <p class="likes-text">Likes:<span class="likes-counter">(0)</span></p>
+    </div>
     </div>
     <button class="button-style" id="main-comment-popup" type="button">Comments</button>`;
     homePage.appendChild(episodeTile);
+  });
+};
+export const displayLikes = (likes) => {
+  likes.forEach((like) => {
+    const tile = document.getElementById(like.item_id);
+    if (tile !== null) {
+      const likeCounter = tile.querySelector('.likes-counter');
+      likeCounter.textContent = `(${like.likes})`;
+    }
   });
 };
