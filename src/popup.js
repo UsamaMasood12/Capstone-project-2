b8c6eb1b311d000a0b80c43f05db93e23b109ce2
josue josuerushanika <100031798+josuerushanika@users.noreply.github.com>
2023-02-15 08:46:52 -0800

    Update async function

--- src/popup.js
+++ src/popup.js
@@ -1,3 +1,11 @@
 const baseUrl = 'https://api.tvmaze.com/search/shows?q=girls';
 
-const button = document.querySelector('btn-container');
+const cmtbutton = document.querySelectorAll('.btn-container');
+
+const aniApi = async () => {
+    const api = await fetch('${baseUrl}');
+    const { data } = await api.json();
+    return data;
+};
+
+const popup
